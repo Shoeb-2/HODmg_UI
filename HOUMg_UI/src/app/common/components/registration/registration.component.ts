3924ea34ccb6ab8320d826a769ff9279ae376a7a
Shoeb-1 <shoeb.ahmed@walkingtree.tech>
2022-09-25 14:51:06 +0530

    updated code for HOD resgistration

--- HOUMg_UI/src/app/common/components/registration/registration.component.ts
+++ HOUMg_UI/src/app/common/components/registration/registration.component.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl,FormGroupDirective,FormBuilder,NgForm, Validators } from '@angular/forms';
 import { CustomValidators } from 'src/app/common/customValidator';
 import { tap } from 'rxjs';
 import { Router } from '@angular/router';
-
+interface department {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  registerForm = new FormGroup({
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    username: new FormControl(null, [Validators.required]),
-    firstname: new FormControl(null, [Validators.required]),
-    lastname: new FormControl(null, [Validators.required]),
-    password: new FormControl(null, [Validators.required]),
-    passwordConfirm: new FormControl(null, [Validators.required])
-  },
-    // add custom Validators to the form, to make sure that password and passwordConfirm are equal
-    { validators: CustomValidators.passwordsMatching }
-  )
-
-  constructor(private router: Router) { }
+  registerForm: FormGroup;
+  departments: department[] = [
+    {value: 'sports', viewValue: 'sports'},
+    {value: 'IT', viewValue: 'IT'},
+    {value: 'Language', viewValue: 'Language'},
+    {value: 'pcmb', viewValue: 'PCMB'},
+  ]
+  departmentValue : any;
+  constructor(private router: Router,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      username: new FormControl(null, [Validators.required]),
+      name: new FormControl(null, [Validators.required]),
+      contactNO: new FormControl(null, [Validators.required]),
+      department: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      passwordConfirm: new FormControl(null, [Validators.required])
+    },
+      // add custom Validators to the form, to make sure that password and passwordConfirm are equal
+      { validators: CustomValidators.passwordsMatching }
+    )
   }
 
   register() {
