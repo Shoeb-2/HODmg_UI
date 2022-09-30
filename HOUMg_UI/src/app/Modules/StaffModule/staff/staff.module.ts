f4b8a0fd1918dee73b67984d0ebd4061c59e70e0
Shoeb-1 <shoeb.ahmed@walkingtree.tech>
2022-09-30 20:27:38 +0530

    adding Mard card for Leave summary in staff

--- HOUMg_UI/src/app/Modules/StaffModule/staff/staff.module.ts
+++ HOUMg_UI/src/app/Modules/StaffModule/staff/staff.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 import { StaffRoutingModule } from './staff-routing.module';
 import { HomeScreenStaffComponent } from '../components/home-screen-staff/home-screen-staff.component';
 import { LeavemanagementComponent } from '../components/leavemanagement/leavemanagement.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/common/material/material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     HomeScreenStaffComponent,
-    LeavemanagementComponent
+    LeavemanagementComponent,
+    
   ],
   imports: [
     CommonModule,
+    FormsModule,
     StaffRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class StaffModule { }
