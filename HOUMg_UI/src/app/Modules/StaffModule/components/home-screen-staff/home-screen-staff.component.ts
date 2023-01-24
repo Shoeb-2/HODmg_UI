c3a95a054f89dec39c6d97268a0bfa4848689121
Shoeb-1 <shoeb.ahmed@walkingtree.tech>
2023-01-24 10:40:40 +0530

    home screen changes

--- HOUMg_UI/src/app/Modules/StaffModule/components/home-screen-staff/home-screen-staff.component.ts
+++ HOUMg_UI/src/app/Modules/StaffModule/components/home-screen-staff/home-screen-staff.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-home-screen-staff',
   templateUrl: './home-screen-staff.component.html',
   styleUrls: ['./home-screen-staff.component.scss']
 })
-export class HomeScreenStaffComponent implements OnInit {
 
+export class HomeScreenStaffComponent implements OnInit {
+  tiles: Tile[] = [
+    {text: 'Casual Leave', cols: 1, rows: 1},
+    {text: 'Earned Leave', cols: 1, rows: 1},
+    {text: 'Comp Offs', cols: 1, rows: 1}
+  ];
   constructor( private router: Router) { }
   fillerContent:any
   ngOnInit(): void {
-
+   
   }
 
 }
